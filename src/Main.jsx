45457015896aa8d6b0bed1b809c51990589123e4
Chinmay-G <chinmay.the1@gmail.com>
2025-04-29 06:13:52 +0530

    Structuring Components

---
+++ src/Main.jsx
@@ -0,0 +1,6 @@
+import { ListBox } from "./components/ListBox";
+import { WatchedBox } from "./WatchedBox";
+
+export function Main({ children }) {
+  return <main className="main">{children}</main>;
+}
